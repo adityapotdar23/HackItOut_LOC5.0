22761b219684db657375a38f3337cf3605a22bb8
Aditya <adityapotdar20@gmail.com>
2023-03-12 07:51:58 +0530

    Pan card

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:/Users/ADITYA/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 import cv2
 from pyaadhaar.utils import Qr_img_to_text, isSecureQr
-import cv2
+import easyocr
+import re
 from PIL import Image
 from pyaadhaar.utils import Qr_img_to_text, isSecureQr
 from pyaadhaar.decode import AadhaarSecureQr
@@ -16,6 +17,8 @@ app = Flask(__name__)
 
 ALLOWED_EXT = set(['jpg', 'jpeg', 'png', 'jfif'])
 
+name = None
+yob = None
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -29,6 +32,8 @@ def index():
 
 @app.route("/aadhar", methods=['GET', 'POST'])
 def aadhar():
+    global name
+    global yob
     if request.method == 'POST':
         # Get the uploaded file from the HTML form
         file = request.files['aadhar_img']
@@ -54,7 +59,7 @@ def aadhar():
         # Extract the signature from the image
         x, y, w, h = cv2.boundingRect(largest_contour)
 
-        cv2.imwrite('static/images/aadhar.png', img[y-10:y+h+10, x-10:x+w+10])
+        cv2.imwrite('static/images/aadhar.png', img[y-20:y+h+20, x-20:x+w+20])
 
         img_name = 'aadhar.png'
         # Extract Aadhaar card details from the image
@@ -66,7 +71,7 @@ def aadhar():
             for i in qrData:
                 root = ET.fromstring(i)
                 uid = root.attrib['uid']
-                name = root.attrib['name']
+                name = root.attrib['name'].lower()
                 gender = root.attrib['gender']
                 yob = root.attrib['yob']
                 context = {"UID": uid, "Name": name,
@@ -78,53 +83,73 @@ def aadhar():
     return render_template('aadhar.html', context=context, img_name=img_name)
 
 
+
 @app.route('/pan', methods=['GET', 'POST'])
 def pan():
+    global name
+    global yob
     if request.method == 'POST':
-
-        file = request.files['aadhar_img'] 
-
+        # Get the uploaded file from the HTML form
+        file = request.files['pan_img']
+        # Read the image file using OpenCV
         img = cv2.imdecode(np.fromstring(
             file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
         
+        cv2.imwrite('static/images/pan_img.png', img)
+
+        pan_img = 'pan_img.png'
+        # # Save the uploaded image temporarily for debugging purposes
+        dpi = 80
+        fig_width, fig_height = int(img.shape[0]/dpi), int(img.shape[1]/dpi)
+        mylst = []
+
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
+        reader = easyocr.Reader(['en']) 
+        result = reader.readtext(img) 
 
-        text1 = pytesseract.image_to_data(threshed,output_type='data.frame')
+        for (bbox, text, prob) in result:
+            if prob >= 0.5:
+                # display 
+                mylst.append(text)
+                print(f'Detected text: {text} (Probability: {prob:.2f})')
 
-        text2 = pytesseract.image_to_string(threshed, lang="ind")
+                # get top-left and bottom-right bbox vertices
+                (top_left, top_right, bottom_right, bottom_left) = bbox
+                top_left = (int(top_left[0]), int(top_left[1]))
+                bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
 
-        text = text1[text1.conf != -1]
+                # create a rectangle for bbox display
+                cv2.rectangle(img=img, pt1=top_left, pt2=bottom_right, color=(255, 0, 0), thickness=10)
 
-        lines = text.groupby('block_num')['text'].apply(list)
+                # put recognized text
+                cv2.putText(img=img, text=text, org=(top_left[0], top_left[1] - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255, 0, 0), thickness=1)
 
-        mylst = [] 
-        for i in lines:
-            mylst.extend(i)
+        if len(mylst) == 0:
+            context = None
+        else:
+            # Parse the XML document
+            mylst_1 = []
+            for i in mylst: 
+                data = re.sub(r'[^a-zA-Z0-9\s]', ' ', i) 
+                data = data.lower()  
+                mylst_1.append(data)
+            mylst_1[ : ] = [' '.join(mylst_1[ : ])]
+            status = 'Reupload your correct PAN image' 
+            if 'income' in mylst_1[0]:
+                status = "Pan image uploaded successully" 
+            if name in mylst_1[0] and yob in mylst_1[0]:
+                verified = 'Pan card and Aaadhar card verified'
+            else:
+                verified = 'Re-upload Pan Card. Not Verified' 
+            context = {'pan_card_status': status, 'verified_status':verified}
 
-        if(len(mylst)==0): 
-            context = None 
-        else: 
-            pnr = ""
-            name = ""
-            yob = ""
-            if 'Number' in mylst: 
-                ind = mylst.index('Number') 
-                pnr += mylst[ind + 1]
-            if 'Name' in mylst: 
-                ind = mylst.index('Name') 
-                name = name + mylst[ind + 1] + " " 
-                name = name + mylst[ind + 2] + " " 
-            if 'Birth' in mylst: 
-                ind = mylst.index('Birth') 
-                yob = yob + mylst[ind + 1]
-            context = {"PNR": pnr, "Name": name, "YOB": yob}
-    else: 
+    else:
         context = None
+        pan_img = None
 
-
-        return render_template('pan.html', context=context)
+    return render_template('pan.html', context=context, pan_img=pan_img)
 
 
 if __name__ == '__main__':
